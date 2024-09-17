3bb3e39674c18b680bf865dd0ab354eee67e91d8
jony37 <156230610+jony37@users.noreply.github.com>
2024-09-17 21:34:35 +0500

    Add files via upload

---
+++ jss/assest/js/app.js
@@ -0,0 +1,55 @@
+"use strict";
+// 1
+// const Users = {
+//   div: document.querySelectorAll('.div'),
+//   btn: document.querySelector('.btn')
+// }
+
+// Users.btn.addEventListener('click', function () {
+//   Users.div.forEach((element, index) => {
+//     element.innerHTML = `item${index}`
+//   });
+// })
+
+// --------------------2------------------------
+
+// const userObj = [
+//  document.querySelector(".inName").value,
+//  document.querySelector(".inPas").value,
+//  document.querySelector('.btn'),
+//  document.querySelector('.text')
+// ];
+
+// const [inNameValue, inPasword, btn, text] = userObj
+
+
+// class Car {
+//   constructor(username, password) {
+//     this.username = username;
+//     this.password = password;
+//   }
+
+//   Info() {
+
+//    btn.addEventListener('click', function () {
+//       const userObj2 = {
+//         inName: document.querySelector(".inName").value,
+//         inPas: document.querySelector(".inPas").value,
+//       };
+
+//       let uname = 'Joha';
+//       let uparol = '123456'
+
+//       if (userObj2.inName === uname && userObj2.inPas === uparol) {
+//         text.innerHTML = `Hush kelibsiz ${userObj2.inName}`
+//       } else if (userObj2.inName === '' && userObj2.inPas === '') {
+//         alert('Iltimos inputni toldiring')
+//       } else {
+//         alert('Username && Parol notugri')
+//       }  
+//     })
+//   }
+// }
+
+// const user = new Car(inNameValue, inPasword);
+// user.Info();
